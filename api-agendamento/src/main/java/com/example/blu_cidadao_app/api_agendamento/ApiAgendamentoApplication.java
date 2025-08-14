20ab67182288c1b4d65aced90443bafc51e7e112
LucasMIepsen <lucasmiepsen@gmail.com>
2025-08-14 09:24:19 -0300

    Merge branch 'main' of https://github.com/LucasMIepsen/api-agendamento.git

---
+++ api-agendamento/src/main/java/com/example/blu_cidadao_app/api_agendamento/ApiAgendamentoApplication.java
@@ -0,0 +1,13 @@
+package com.example.blu_cidadao_app.api_agendamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAgendamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAgendamentoApplication.class, args);
+	}
+
+}
